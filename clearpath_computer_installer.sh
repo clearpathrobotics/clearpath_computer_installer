8df68a969dad890a856bb34ee27af1593da58dff
Luis Camero <lcamero@clearpathrobotics.com>
2024-08-16 12:21:31 -0400

    Add Dingo and Ridgeback options

--- clearpath_computer_installer.sh
+++ clearpath_computer_installer.sh
@@ -229,7 +229,7 @@ if [ ! "$EUID" -eq 0 ]; then
   if [[ $ROBOT_CHOICE -eq -1 ]];
   then
     echo ""
-    prompt_option ROBOT_CHOICE "Which robot are you installing?" "Clearpath Husky A200" "Clearpath Jackal J100" "Clearpath Warthog W200"
+    prompt_option ROBOT_CHOICE "Which robot are you installing?" "Clearpath Husky A200" "Clearpath Jackal J100" "Clearpath Warthog W200" "Clearpath Warthog DD100" "Clearpath Warthog DD150" "Clearpath Warthog DO100" "Clearpath Warthog DO150" "Clearpath Warthog R100"
   fi
   case "$ROBOT_CHOICE" in
     1)
@@ -240,6 +240,21 @@ if [ ! "$EUID" -eq 0 ]; then
       ;;
     3)
       platform="w200"
+      ;;
+    4)
+      platform="dd100"
+      ;;
+    5)
+      platform="dd150"
+      ;;
+    6)
+      platform="do100"
+      ;;
+    7)
+      platform="do150"
+      ;;
+    8)
+      platform="r100"
       ;;
     * )
       echo -e "\e[31mERROR: Invalid selection"
